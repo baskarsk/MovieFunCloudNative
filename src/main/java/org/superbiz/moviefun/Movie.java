2594d8a0299d300ce406134bfb6fafcd26185ce5
baskarsk <Baskar.Saravanan@cognizant.com>
2018-09-19 15:14:37 +0530

    Added Table entity with the name movies

--- src/main/java/org/superbiz/moviefun/Movie.java
+++ src/main/java/org/superbiz/moviefun/Movie.java
@@ -23,6 +23,7 @@ import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity
+@Table(name = "movies")
 public class Movie implements Serializable {
 
     private static final long serialVersionUID = 1L;
